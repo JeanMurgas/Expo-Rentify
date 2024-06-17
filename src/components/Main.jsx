bee9ebf41149b46036a5c42528fc5d7981074b9e
JeanMurgas <jeancamurgas30@gmail.com>
2024-06-16 19:17:17 -0500

    start

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View} from 'react-native'
+
+const Main = () => {
+    return(
+        <View>
+            <Text>Esta es la pantalla de Home</Text>
+        </View>
+    )
+}
+
+export default Main
